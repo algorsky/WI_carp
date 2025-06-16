e88841ad599766a3da114194b73c0ce2a7b3a16f
Adrianna Gorsky <adriannagorsky@Adriannas-MacBook-Air-2.attlocal.net>
2025-06-16 13:35:05 -0500

    updated macrophyte

--- 0_shannon.R
+++ 0_shannon.R
@@ -1,13 +1,13 @@
 library(tidyverse)
+library(vegan)
 
 inUrl1  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/23/30/aa5720aec0e577431faeee352b91a937" 
 infile1 <- tempfile()
 try(download.file(inUrl1,infile1,method="curl"))
 
+#Organize data for all depths
 d1<- read_csv(infile1)|>
   filter(lakeid == "WI")
-
-crosswalk<- read_csv("data/crosswork_macrophyte.csv")
 
 df<- d1%>%
   filter(spname != "UNKNOWN"  & spname != "UNKNOWN 1")%>%
@@ -17,19 +17,14 @@ df<- d1%>%
   mutate(spname = ifelse(spname == "POTAMOGETON NATANS:", "POTAMOGETON NATANS", spname))%>%
   mutate(spname = ifelse(spname == "NYMPHAEA ODORATA SSP. TUBEROSA", "NYMPHAEA ODORATA",
                          ifelse(spname == "NUPHAR VARIEGATA", "NYMPHAEA ODORATA", spname)))%>%
+  mutate(spname = ifelse(spname == "STRUCKENIA PECTINATA", "STUCKENIA PECTINATA", spname))%>%
+  mutate(spname = ifelse(spname == "STUCKENIA PECTINATA", "Potamogeton pectinatus L.", spname))%>%
+  mutate(spname = ifelse(spname == "BRASENIA SCHREBERI:", "BRASENIA SCHREBERI", spname))%>%
   dplyr::select(-flag)
 
-df_crosswalk<- df%>%
-  left_join(crosswalk, by = "spname")%>%
-  mutate(spname = ifelse(spname == "STRUCKENIA PECTINATA", "STUCKENIA PECTINATA", spname))%>%
-  mutate(name = ifelse(spname == "STUCKENIA PECTINATA", "Potamogeton pectinatus L.", name))%>%
-  mutate(label = ifelse(name %in% NA, "other", name))
-
-#Depth below 2
 # Group by year and species, then count occurrences
-species_counts_by_year <- df_crosswalk %>%
-  filter(depth < 2.5)%>%
-  group_by(year4, label) %>%
+species_counts_by_year <- df %>%
+  group_by(year4, spname) %>%
   summarize(Count = n(), .groups = 'drop')
 
 # Calculate the total number of occurrences per year
@@ -48,37 +43,13 @@ species_counts_with_frequencies <- species_counts_with_totals %>%
 #decending order
 species_counts_with_frequencies <- species_counts_with_frequencies %>%
   arrange(year4, desc(Relative_Frequency))%>%
-  mutate(Relative_Frequency_perc = Relative_Frequency*100)
+  mutate(Relative_Frequency_perc =Relative_Frequency*100 )
 
 table_frequ<- species_counts_with_frequencies %>% 
-  dplyr::select(year4, label, Relative_Frequency_perc)%>%
+  dplyr::select(year4, spname, Relative_Frequency_perc)%>%
   pivot_wider(names_from = year4, values_from = Relative_Frequency_perc)
 
-#combine Potamogeton praelongus and richardsonii
-#inclusive of 2m and below
-
-#Average based off year groupings
-data_category<- species_counts_with_frequencies%>%
-  mutate(Year = as.numeric(year4))%>%
-  mutate(year = ifelse(Year < 1998, "1995-1997",
-                       ifelse(Year > 1997 & Year < 2001, "1998-2000",
-                              ifelse(Year > 2000 & Year < 2004, "2001-2003",
-                                     ifelse(Year > 2003 & Year < 2008, "2004-2007",
-                                            ifelse(Year > 2007 & Year < 2011, "2008-2010",
-                                                   ifelse(Year > 2010 & Year < 2014, "2011-2013",
-                                                          ifelse(Year > 2013 & Year < 2017, "2014-2016", "2017-2018"))))))))
-
-data_category_summary<- data_category%>%
-  group_by(year, label)%>%
-  summarize(frequency = mean(Relative_Frequency)*100)
-
-table_mean_freq<- data_category_summary %>% 
-  dplyr::select(year, label, frequency)%>%
-  pivot_wider(names_from = year, values_from = frequency)
-
-
 #Shannon
-library(vegan)
 
 # Select only numeric columns (exclude 'label')
 df2_numeric <- table_frequ[, -1]  # or df2 %>% select(-label)
@@ -94,3 +65,20 @@ shannon_index_df <- data.frame(
   year = names(shannon_index),
   shannon = as.numeric(shannon_index)
 )
+
+#Average based off year groupings
+data_category<- shannon_index_df%>%
+  mutate(Year = as.numeric(year))%>%
+  mutate(year = ifelse(Year < 1998, "1995-1997",
+                       ifelse(Year > 1997 & Year < 2001, "1998-2000",
+                              ifelse(Year > 2000 & Year < 2004, "2001-2003",
+                                     ifelse(Year > 2003 & Year < 2008, "2004-2007",
+                                            ifelse(Year > 2007 & Year < 2011, "2008-2010",
+                                                   ifelse(Year > 2010 & Year < 2014, "2011-2013",
+                                                          ifelse(Year > 2013 & Year < 2017, "2014-2016", "2017-2018"))))))))
+
+data_category_summary<- data_category%>%
+  group_by(year)%>%
+  summarize(shannon = mean(shannon))
+
+
